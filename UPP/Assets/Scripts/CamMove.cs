b6534f130f8e267001c079be7ad650108cacecb2
vlad9121 <aloev.vlad2191@yandex.ru>
2021-10-17 20:38:22 +0300

    download
    Залил проект

---
+++ UPP/Assets/Scripts/CamMove.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+    public GameObject player;
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
+    }
+}
